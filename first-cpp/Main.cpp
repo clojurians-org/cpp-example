3819d93817f0b1afcd1ec0b6eb648b0a80ee2b51
clojurians-org <larluo@clojurians.org>
2018-06-25 18:24:07 +0800

    add boost library test

---
+++ first-cpp/Main.cpp
@@ -0,0 +1,16 @@
+#include <pthread.h> 
+#include <boost/process.hpp>
+// #include <boost/program_options.hpp>
+// #include <boost/version.hpp>
+
+#include <iostream>
+
+// namesapce po = boost::program_options ;
+
+namespace firstCpp {
+}
+int main (int argc, char** argv) {
+  std::cout << ("Hello World") << std::endl ;
+  return 0 ;
+}
+
